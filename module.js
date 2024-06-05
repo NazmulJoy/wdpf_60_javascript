8f3c5f9d172194b105eb56007ba2511492f3c90d
NazmulJoy <Nazmuljoys@gmail.com>
2024-06-05 13:01:33 +0600

    05june24

---
+++ module.js
@@ -0,0 +1,10 @@
+const student_info = () => {
+    let name = "Rahim";
+    let address = "Mirpur";
+    return `Name: ${name} <br> Address: ${address}`; 
+}
+let country = "Bangladesh";
+let district = "Dhaka";
+
+export default student_info;
+export { country, district };
