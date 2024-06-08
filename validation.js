4724dc15c81c974359a598a0763ab0a408520714
NazmulJoy <Nazmuljoys@gmail.com>
2024-06-08 13:14:37 +0600

    8june24

---
+++ validation.js
@@ -0,0 +1,14 @@
+class Validation {
+    constructor(email) {
+        this.email= email;
+    }
+    validate(){
+         var pattern =/[A-z0-9._]+@[A-z0-9]+\.[A-z]{2,4}/;
+         if(this.email.search(pattern)==-1){
+            alert("Please enter a valid email address");
+         } 
+         else {
+            alert("valid email Address");
+         }
+    }
+}
